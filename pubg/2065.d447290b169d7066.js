145203176e0f691d44e15afeb1b695fe5b2a1e35
Bhavya Sharma <thebhavya85@gmail.com>
2026-03-13 17:26:23 +0530

    Plsssss

---
+++ pubg/2065.d447290b169d7066.js
@@ -0,0 +1,103 @@
+"use strict";
+(self.webpackChunkapp = self.webpackChunkapp || []).push([
+    [2065], {
+        2065: (p, l, a) => {
+            a.r(l), a.d(l, {
+                BlogPageModule: () => h
+            });
+            var c = a(177),
+                d = a(4341),
+                e = a(4742),
+                r = a(8986),
+                o = a(4438);
+            const g = [{
+                path: "",
+                component: (() => {
+                    var n;
+                    class t {
+                        constructor() {}
+                        ngOnInit() {}
+                    }
+                    return (n = t).\u0275fac = function(i) {
+                        return new(i || n)
+                    }, n.\u0275cmp = o.VBU({
+                        type: n,
+                        selectors: [
+                            ["app-blog"]
+                        ],
+                        decls: 186,
+                        vars: 2,
+                        consts: [
+                            [1, "ion-page"],
+                            [2, "background-color", "#00796b", "border-radius", "10px 100px / 120px"],
+                            [1, "ion-hide-md-up", 2, "display", "flex", "justify-content", "start"],
+                            [2, "color", "white"],
+                            [1, "ion-hide-md-down", 2, "display", "flex", "justify-content", "center"],
+                            [2, "display", "flex"],
+                            ["routerLink", "/home", 2, "color", "white"],
+                            ["routerLink", "/about", 2, "color", "white"],
+                            ["routerLink", "/procedure", 2, "color", "white"],
+                            ["routerLink", "/doctor", 2, "color", "white"],
+                            ["routerLink", "/gallery", 2, "color", "white"],
+                            ["routerLink", "/blog", 2, "color", "white"],
+                            ["routerLink", "/contact", 2, "color", "white"],
+                            [2, "width", "100%"],
+                            [2, "z-index", "10000", "position", "fixed", "bottom", "20px", "right", "20px"],
+                            ["target", "_blank"],
+                            ["alt", "", 2, "width", "50px", "height", "50px", 3, "src"],
+                            ["size", "12", "offsetLg", "1", "sizeLg", "10"],
+                            ["size", "12", "offsetLg", "2", "sizeLg", "8"],
+                            ["alt", "", 3, "src"],
+                            ["size", "12"],
+                            ["offsetLg", "2", "sizeLg", "8", "size", "12"],
+                            ["size", "12", "sizeLg", "6"],
+                            [1, "footer_section", "layout_padding"],
+                            ["size", "12", "sizeLg", "4"],
+                            ["routerLink", "/about", 2, "color", "white", "font-size", "medium"],
+                            ["routerLink", "/procedure", 2, "color", "white", "font-size", "medium"],
+                            ["routerLink", "/doctor", 2, "color", "white", "font-size", "medium"],
+                            ["routerLink", "/gallery", 2, "color", "white", "font-size", "medium"],
+                            ["routerLink", "/blog", 2, "color", "white", "font-size", "medium"],
+                            ["name", "location-outline", 2, "color", "white", "width", "25px", "height", "25px"],
+                            ["name", "call-outline", 2, "color", "white", "width", "25px", "height", "25px"],
+                            [2, "display", "flex", "justify-content", "center"],
+                            ["href", "https://www.facebook.com/share/1D4KBWSDdJ/", "target", "_blank"],
+                            ["name", "logo-facebook", 2, "color", "white", "width", "30px", "height", "30px"],
+                            ["name", "logo-twitter", 2, "color", "white", "width", "30px", "height", "30px"],
+                            ["name", "logo-linkedin", 2, "color", "white", "width", "30px", "height", "30px"],
+                            [1, "copyright_text"],
+                            ["href", "https://www.ftstsolution.com/", "target", "_blank"]
+                        ],
+                        template: function(i, m) {
+                            1 & i && (o.j41(0, "div", 0)(1, "ion-header")(2, "ion-grid")(3, "ion-row", 1)(4, "ion-col", 2), o.nrm(5, "ion-menu-button", 3), o.k0s(), o.j41(6, "ion-col", 4)(7, "ul", 5)(8, "li")(9, "a", 6), o.EFF(10, "Home"), o.k0s()(), o.j41(11, "li")(12, "a", 7), o.EFF(13, "About Us"), o.k0s()(), o.j41(14, "li")(15, "a", 8), o.EFF(16, "Procedure"), o.k0s()(), o.j41(17, "li")(18, "a", 9), o.EFF(19, "Doctors"), o.k0s()(), o.j41(20, "li")(21, "a", 10), o.EFF(22, "Gallery"), o.k0s()(), o.j41(23, "li")(24, "a", 11), o.EFF(25, "Blog"), o.k0s()(), o.j41(26, "li")(27, "a", 12), o.EFF(28, "Contact Us"), o.k0s()()()()()()(), o.j41(29, "ion-content", 13)(30, "div", 14)(31, "a", 15), o.nrm(32, "ion-img", 16), o.k0s()(), o.j41(33, "ion-grid")(34, "ion-row")(35, "ion-col", 17)(36, "ion-card")(37, "ion-card-header")(38, "ion-grid")(39, "ion-row")(40, "ion-col", 18), o.nrm(41, "ion-img", 19), o.k0s()()()(), o.j41(42, "ion-card-content")(43, "ion-grid")(44, "ion-row")(45, "ion-col", 20)(46, "p"), o.EFF(47, " Kidney stones, also known as renal calculi, are a common condition that affects both men and women. These stones form when minerals and salts crystallize in the kidneys, causing serious health issues such as infections, kidney failure, and damage. Preventing kidney stones is essential for maintaining kidney health and improving quality of life. The good news is that with the right lifestyle and dietary changes, you can significantly reduce the risk of developing kidney stones. "), o.k0s()(), o.j41(48, "ion-col", 20)(49, "h2"), o.EFF(50, "Table of Contents"), o.k0s()(), o.j41(51, "ion-col", 21)(52, "ol")(53, "ion-grid")(54, "ion-row")(55, "ion-col", 22)(56, "li"), o.EFF(57, "Stay Hydrated"), o.k0s(), o.j41(58, "li"), o.EFF(59, "Limit Sodium Intake"), o.k0s(), o.j41(60, "li"), o.EFF(61, "Consume Dietary Calcium"), o.k0s(), o.j41(62, "li"), o.EFF(63, "Reduce Foods with High Oxalate"), o.k0s()(), o.j41(64, "ion-col", 22)(65, "li"), o.EFF(66, "Avoid Added Sugars"), o.k0s(), o.j41(67, "li"), o.EFF(68, "Eat Less Animal Protein"), o.k0s(), o.j41(69, "li"), o.EFF(70, "Consume a Healthy Diet"), o.k0s(), o.j41(71, "li"), o.EFF(72, "Consult with Your Doctor"), o.k0s()()()()()(), o.j41(73, "ion-col", 20)(74, "h3"), o.EFF(75, "1. Stay Hydrated"), o.k0s()(), o.j41(76, "ion-col", 20)(77, "p"), o.EFF(78, " Drinking plenty of fluids, especially water, is one of the most effective ways to prevent kidney stones. Staying hydrated helps to dilute the urine and prevent minerals from crystallizing. Aim to drink at least 8 glasses or 3 liters of fluids daily, including water, buttermilk, orange juice, and lemonade. The color of your urine can indicate hydration levels: clear, pale yellow urine suggests proper hydration, while dark and concentrated urine is a sign to drink more fluids. "), o.k0s()(), o.j41(79, "ion-col", 20)(80, "h3"), o.EFF(81, "2. Limit Sodium Intake"), o.k0s()(), o.j41(82, "ion-col", 20)(83, "p"), o.EFF(84, " Excess sodium can lead to dehydration and increase the risk of kidney stone formation. Reducing sodium-rich foods, such as processed foods, fast food, canned soups, pickles, and condiments, is crucial. The American Heart Association recommends no more than 2,300 milligrams of sodium daily, which is approximately one teaspoon of salt. "), o.k0s()(), o.j41(85, "ion-col", 20)(86, "h3"), o.EFF(87, "3. Consume Dietary Calcium"), o.k0s()(), o.j41(88, "ion-col", 20)(89, "p"), o.EFF(90, " Calcium is often associated with kidney stone formation, but it\u2019s essential to consume adequate calcium to reduce the risk of stones, especially calcium-oxalate stones. Instead of limiting calcium intake, focus on incorporating healthy sources like milk, cheese, yogurt, and tofu into your diet. "), o.k0s()(), o.j41(91, "ion-col", 20)(92, "h3"), o.EFF(93, "4. Reduce Foods with High Oxalate"), o.k0s()(), o.j41(94, "ion-col", 20)(95, "p"), o.EFF(96, " Oxalate-rich foods can contribute to kidney stones by binding with calcium in the urine. To reduce the risk, limit foods such as spinach, chocolate, tea, nuts, and sweet potatoes. Combining calcium-rich foods with oxalate-rich foods may help prevent stones from forming in the kidneys. "), o.k0s()(), o.j41(97, "ion-col", 20)(98, "h3"), o.EFF(99, "5. Avoid High Doses of Vitamin C Supplements"), o.k0s()(), o.j41(100, "ion-col", 20)(101, "p"), o.EFF(102, " Excessive Vitamin C intake (1000 mg or more daily) can increase the risk of kidney stones, especially in people with a history of calcium-oxalate stones. Stick to the recommended daily intake: 75 mg for women and 90 mg for men. Get your Vitamin C from natural sources like fruits and vegetables for better safety. "), o.k0s()(), o.j41(103, "ion-col", 20)(104, "h3"), o.EFF(105, "6. Avoid Added Sugars"), o.k0s()(), o.j41(106, "ion-col", 20)(107, "p"), o.EFF(108, " Added sugars in processed foods, sugary drinks, and sweets can contribute to kidney stone formation. These sugars can reduce urine volume and increase calcium levels in the urine. Limiting added sugar intake is key to preventing kidney stones. The recommended daily limit is 6 teaspoons for women and 9 teaspoons for men. "), o.k0s()(), o.j41(109, "ion-col", 20)(110, "h3"), o.EFF(111, "7. Eat Less Animal Protein"), o.k0s()(), o.j41(112, "ion-col", 20)(113, "p"), o.EFF(114, " Consuming too much animal protein can lead to higher uric acid and calcium levels in the urine, increasing the risk of kidney stones. Consider reducing your intake of animal-based proteins like eggs, meat, and fish, and include more plant-based protein sources such as legumes, beans, and lentils. "), o.k0s()(), o.j41(115, "ion-col", 20)(116, "h3"), o.EFF(117, "8. Consume a Healthy Diet"), o.k0s()(), o.j41(118, "ion-col", 20)(119, "p"), o.EFF(120, " A diet rich in fruits and vegetables helps maintain overall health and reduce the risk of kidney stones. Limit processed foods, fast food, and sugary snacks while focusing on a balanced diet that supports kidney function and overall well-being. "), o.k0s()(), o.j41(121, "ion-col", 20)(122, "h3"), o.EFF(123, "9. Consult with Your Doctor"), o.k0s()(), o.j41(124, "ion-col", 20)(125, "p"), o.EFF(126, " If you are on any prescribed medications, it\u2019s important to consult your doctor, as some drugs can increase the risk of kidney stones. A healthcare professional can offer guidance on treatment options and preventive measures specific to your condition. "), o.k0s()(), o.j41(127, "ion-col", 20)(128, "h3"), o.EFF(129, "Conclusion"), o.k0s()(), o.j41(130, "ion-col", 20)(131, "p"), o.EFF(132, " Adopting these 9 simple steps can significantly reduce your risk of developing kidney stones. While there is no guarantee, a proper diet, hydration, and lifestyle adjustments can go a long way in preventing kidney stones and supporting overall kidney health. At New Patna Uro Gynee and Stone Hospital, we provide expert consultations and treatment options to help you manage kidney stone risks and maintain a healthy lifestyle. "), o.k0s()()()()()()()(), o.j41(133, "ion-row", 23)(134, "ion-col", 24)(135, "div")(136, "h4", 3), o.EFF(137, "Useful Links"), o.k0s()(), o.j41(138, "div")(139, "ul")(140, "li")(141, "a", 25), o.EFF(142, "About"), o.k0s()(), o.j41(143, "li")(144, "a", 26), o.EFF(145, "Procedures"), o.k0s()(), o.j41(146, "li")(147, "a", 27), o.EFF(148, "Doctors"), o.k0s()(), o.j41(149, "li")(150, "a", 28), o.EFF(151, "Gallery"), o.k0s()(), o.j41(152, "li")(153, "a", 29), o.EFF(154, "Blog"), o.k0s()()()()(), o.j41(155, "ion-col", 24)(156, "h4", 3), o.EFF(157, "Contact Us"), o.k0s(), o.j41(158, "div", 5), o.nrm(159, "ion-icon", 30), o.j41(160, "p", 3), o.EFF(161, " Opposite Tribhuwan School, Vijay Singh Yadav Path, Ashopur, Khagaul Road, Patna "), o.k0s()(), o.j41(162, "div", 5), o.nrm(163, "ion-icon", 31), o.j41(164, "p", 3), o.EFF(165, "7481943963"), o.k0s(), o.j41(166, "p", 3), o.EFF(167, "8789505757"), o.k0s()()(), o.j41(168, "ion-col", 24)(169, "h4", 3), o.EFF(170, "Get in Touch"), o.k0s(), o.j41(171, "div", 32)(172, "a", 33), o.nrm(173, "ion-icon", 34), o.k0s(), o.j41(174, "a", 33), o.nrm(175, "ion-icon", 35), o.k0s(), o.j41(176, "a", 33), o.nrm(177, "ion-icon", 36), o.k0s()()(), o.j41(178, "ion-col", 20)(179, "p", 37), o.EFF(180, " Copyright \xa9 2025 Patna Uro Gynae & Stone Hospital | Powered by Patna Uro Gynae & Stone Hospital "), o.k0s()(), o.j41(181, "ion-col", 20)(182, "p", 37), o.EFF(183, " Design by: "), o.j41(184, "a", 38), o.EFF(185, "FTST Solution Pvt. Ltd."), o.k0s()()()()()()()), 2 & i && (o.R7$(32), o.Y8G("src", "https://ftstsolution.com/files/devData/WhatsApp_icon.png"), o.R7$(9), o.Y8G("src", "assets/icon/KidneyStone.jpeg"))
+                        },
+                        dependencies: [e.b_, e.I9, e.ME, e.hU, e.W9, e.lO, e.eU, e.iq, e.KW, e.MC, e.ln, e.oY, r.Wk],
+                        styles: [".navbar[_ngcontent-%COMP%]   ul[_ngcontent-%COMP%]{text-align:center;list-style:none;margin:0;padding:0}.navbar[_ngcontent-%COMP%]   li[_ngcontent-%COMP%]{margin:10px}a[_ngcontent-%COMP%]{text-decoration:none;color:#3880ff;font-size:16px;font-weight:700;transition:color .3s ease}.navbar[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover{color:#1e62d0}ion-card[_ngcontent-%COMP%]{background-color:beige}h2[_ngcontent-%COMP%]{font-family:Times New Roman;font-weight:700;color:#036;font-size:50px;text-align:center;margin-top:20px}h3[_ngcontent-%COMP%]{font-family:Times New Roman;font-weight:700;color:#00796b;font-size:40px;text-align:center;margin-top:20px}h4[_ngcontent-%COMP%]{color:#0d47a1;text-align:center;font-family:sans-serif;font-weight:700}strong[_ngcontent-%COMP%]{color:#28a745}ion-menu-button[_ngcontent-%COMP%]::part(icon){font-size:35px;width:35px;height:35px}swiper-container[_ngcontent-%COMP%]::part(button-next){font-size:20px;width:20px;height:20px}swiper-container[_ngcontent-%COMP%]::part(button-prev){font-size:20px;width:20px;height:20px}p[_ngcontent-%COMP%]{font-family:sans-serif;font-size:medium;margin:10px auto;font-weight:700;width:100%;text-align:center;color:#333}li[_ngcontent-%COMP%]{font-family:sans-serif;color:#000;font-size:medium}.footer_section[_ngcontent-%COMP%]{width:100%;float:left;background-color:#00796b;height:auto}.layout_padding[_ngcontent-%COMP%]{padding-top:50px;padding-bottom:0}.input_bt[_ngcontent-%COMP%]{width:60%;margin:0 auto;text-align:center;display:flex;border-bottom:1px solid #ffffff!important}.mail_bt[_ngcontent-%COMP%]{width:100%;float:left;font-size:18px;color:#fff;background-color:transparent;padding:5px 0;height:60px;border:0px}input.mail_bt[_ngcontent-%COMP%]::placeholder{color:#fff}.subscribe_bt[_ngcontent-%COMP%]{width:30%;float:right;font-size:16px;color:#f26522;background-color:transparent;padding:20px 0 5px;border-radius:30px;border:0px;text-transform:uppercase;text-align:right}.subscribe_bt[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]{color:#f26522}.subscribe_bt[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover{color:#fff}.footer_menu[_ngcontent-%COMP%]{width:100%;margin:0 auto;text-align:center}.location_main[_ngcontent-%COMP%]{width:100%;margin:0 auto;padding-top:30px;text-align:center}.location_main[_ngcontent-%COMP%]{float:left;font-size:16px;color:#fff;padding:30px}.location_main[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]{color:#fff}.location_main[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover{color:#f26522}.copyright_text[_ngcontent-%COMP%]{width:100%;float:left;color:#fff;text-align:center;font-size:16px;margin:20px 0}.copyright_text[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]{color:#4d4d4d}.copyright_text[_ngcontent-%COMP%]   a[_ngcontent-%COMP%]:hover{color:#f26522}ul[_ngcontent-%COMP%]{text-align:center;list-style:none;margin:0;padding:0}li[_ngcontent-%COMP%]{margin:10px}"]
+                    }), t
+                })()
+            }];
+            let u = (() => {
+                    var n;
+                    class t {}
+                    return (n = t).\u0275fac = function(i) {
+                        return new(i || n)
+                    }, n.\u0275mod = o.$C({
+                        type: n
+                    }), n.\u0275inj = o.G2t({
+                        imports: [r.iI.forChild(g), r.iI]
+                    }), t
+                })(),
+                h = (() => {
+                    var n;
+                    class t {}
+                    return (n = t).\u0275fac = function(i) {
+                        return new(i || n)
+                    }, n.\u0275mod = o.$C({
+                        type: n
+                    }), n.\u0275inj = o.G2t({
+                        imports: [c.MD, d.YN, e.bv, u]
+                    }), t
+                })()
+        }
+    }
+]);
